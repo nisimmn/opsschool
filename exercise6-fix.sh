201e10d7b7ad33d1a2eb14613ebc580418890a5c
nisimmn <99735392+nisimmn@users.noreply.github.com>
2022-07-25 17:53:19 +0300

    Delete exercise6-fix.sh

--- exercise6-fix.sh
+++
@@ -1,7 +0,0 @@
-#This script will copy 2 files to server2"
-
-echo "file to move1:$1"
-echo "file to move2:$2"
-echo "folder destenation on the other server:$3"
-
-rsync  -vr --stats $1 $2 vagrant@server2:/$3  | grep "Number of files"  | awk -F " " '{print $1,$2,$3,$4}'
