eebc0fa6629f13bf1fbf6f902d49b1b1e6916ca8
nisimmn <99735392+nisimmn@users.noreply.github.com>
2022-07-25 17:42:38 +0300

    Add files via upload

---
+++ exercise5-fix_server1.sh
@@ -0,0 +1,2 @@
+echo -e "Host server2\n StrictHostKeyChecking=no" >> ~/.ssh/config
+sudo systemctl restart apache2
---
+++ exercise5-fix_server2.sh
@@ -0,0 +1,2 @@
+echo -e "Host server1\n StrictHostKeyChecking=no" >> ~/.ssh/config
+sudo systemctl restart apache2
