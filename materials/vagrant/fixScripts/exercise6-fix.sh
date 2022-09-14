5604f8fedf77e42fc5d95351c3345e5d68dbbf09
nisimmn <99735392+nisimmn@users.noreply.github.com>
2022-09-14 10:00:58 +0300

    Update exercise6-fix.sh

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+echo "file to move1:$1"
+echo "file to move2:$2"
+echo "folder destenation on the other server:$3"
+
+rsync  -vr --stats $1 $2 vagrant@server2:/$3  | grep "Number of files"  | awk -F " " '{print $1,$2,$3,$4}'
